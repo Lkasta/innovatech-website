06d624af532ada6e2eac9a7f05d7f0a7185a4a23
Lkasta <lkastabackup@gmail.com>
2025-07-16 14:03:29 -0300

    feat: initial commit with vite + tailwindcss + shadcn

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="bg-zinc-800 flex items-center justify-center w-full min-h-svh">
+      <h1 className="text-4xl text-white font-bold">Innovatech</h1>
+    </main>
+  );
+}
+
+export default App;
