76148743e1ae09a4bd45f7bc51d89a08b00c7468
Lkasta <lkastabackup@gmail.com>
2025-07-21 14:42:09 -0300

    feat: refactor contacts

--- src/components/BrasilBanner.tsx
+++ src/components/BrasilBanner.tsx
@@ -1,6 +1,8 @@
-import { CircleUserRound } from "lucide-react";
-import { Button } from "./ui/button";
+import { Mail, MapPin } from "lucide-react";
+import { UserCircle } from "@phosphor-icons/react";
+import { MessageCircle } from "lucide-react";
 import BrasilVetor from "/public/img/brasil-sp-highlight.svg";
+import { Button } from "./ui/button";
 
 export default function BrasilBanner() {
   return (
@@ -16,19 +18,42 @@ export default function BrasilBanner() {
             melhores soluções.
           </p>
         </div>
-        <img className="w-[450px] select-none" src={BrasilVetor} alt="Brasil vetor focus in São Paulo" />
-        <div className="text-main-blue">
-          <div className="flex gap-3">
-            <h1 className="text-main-blue text-3xl font-bold">São Paulo</h1>
-            <Button className="border-2 border-gray-400 bg-gray-200 text-gray-400">
-              Localização no mapa
-            </Button>
+        <img
+          className="w-[450px] select-none"
+          src={BrasilVetor}
+          alt="Brasil vetor focus in São Paulo"
+        />
+        <div className="text-main-blue space-y-6">
+          <div className="flex flex-col space-y-1">
+            <h1 className="text-main-blue text-3xl font-bold">
+              Itupeva - São Paulo
+            </h1>
+            <a
+              href="https://maps.app.goo.gl/nnc5tczNXQhgreSS7"
+              target="_blank"
+              className="hover:text-main-blue flex gap-1 text-gray-400 underline transition-all"
+            >
+              <MapPin width={16} />
+              <p>Localização no mapa</p>
+            </a>
           </div>
-          <div className="w-min rounded-lg bg-gray-200 p-2">
-            <CircleUserRound width={25} height={25} className="" />
+          <div className="flex w-min items-center gap-2 rounded-lg bg-gray-200 p-2">
+            <UserCircle size={40} weight="fill" className="text-gray-600" />
             <div className="">
-              <h2>Innovatech - Itupeva</h2>
-              <p></p>
+              <h2 className="font-bold text-nowrap text-gray-600">
+                Innovatech - Itupeva
+              </h2>
+              <p className="text-xs text-nowrap text-gray-600">
+                (11) 95344-7434 - jsilva@innovatech24.com
+              </p>
+            </div>
+            <div className="flex gap-3 ml-2">
+              <Button className="bg-main-blue">
+                <MessageCircle className="text-white" />
+              </Button>
+              <Button className="bg-main-blue">
+                <Mail className="text-white" />
+              </Button>
             </div>
           </div>
         </div>
