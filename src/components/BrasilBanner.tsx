9a2c0a1415d9144d36bb3799b8f0735785af326b
Lkasta <lkastabackup@gmail.com>
2025-07-21 13:50:12 -0300

    feat: experience component

---
+++ src/components/BrasilBanner.tsx
@@ -0,0 +1,41 @@
+import { CircleUserRound } from "lucide-react";
+import { Button } from "./ui/button";
+
+export default function BrasilBanner() {
+  return (
+    <section className="flex w-full -translate-y-52 items-center justify-center py-20">
+      <div className="container grid grid-cols-3 items-center rounded-lg bg-white p-10">
+        <div className="space-y-4">
+          <h1 className="text-3xl font-bold text-gray-800 md:text-4xl">
+            Entre em contato com a nossa equipe
+          </h1>
+          <p className="text-lg text-gray-600">
+            Estamos sempre prontos para oferecer o suporte que você precisa.
+            Conte com a nossa equipe para esclarecer suas dúvidas e encontrar as
+            melhores soluções.
+          </p>
+        </div>
+        <img
+          className="w-[450px] select-none"
+          src="/public/img/brasil-sp-highlight.svg"
+          alt="img"
+        />
+        <div className="text-main-blue">
+          <div className="flex gap-3">
+            <h1 className="text-main-blue text-3xl font-bold">São Paulo</h1>
+            <Button className="border-2 border-gray-400 bg-gray-200 text-gray-400">
+              Localização no mapa
+            </Button>
+          </div>
+          <div className="w-min bg-gray-200 p-2 rounded-lg">
+            <CircleUserRound width={25} height={25} className="" />
+            <div className="">
+              <h2>Innovatech - Itupeva</h2>
+              <p></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/components/Experience.tsx
@@ -0,0 +1,54 @@
+import { ArrowRight } from "lucide-react";
+import { Button } from "./ui/button";
+import BrasilBanner from "./BrasilBanner";
+
+export default function Experience() {
+  return (
+    <section className="relative overflow-hidden bg-gray-100">
+      <div className="container flex w-full">
+        <div className="to-main-blue/25 w-[55%] origin-top-left skew-x-[-6deg] bg-gray-100 bg-gradient-to-t" />
+
+        <div className="mx-auto flex-1 px-4 pt-32 pb-56 lg:px-6">
+          <div className="space-y-5">
+            <h2 className="mb-4 text-5xl font-bold text-gray-800 lg:text-5xl">
+              15 anos de experiência
+            </h2>
+            <div className="space-y-4">
+              <p className="max-w-3xl text-xl text-gray-600">
+                Somos uma empresa especializada em eletrônica e automação
+                industrial com mais de 15 anos de experiência no setor,
+                oferecendo soluções tecnológicas inovadoras e personalizadas.
+              </p>
+              <p className="max-w-3xl text-xl text-gray-600">
+                Nosso compromisso é desenvolver projetos personalizados,
+                realizar manutenções precisas e implementar tecnologias que
+                otimizam processos, reduzem custos e aumentam a produtividade.
+              </p>
+            </div>
+            <Button
+              size="lg"
+              className="bg-main-blue h-12 rounded-lg px-8 py-3 text-white transition-all duration-300 hover:scale-105"
+            >
+              <p className="select-none">Solicitar Orçamento</p>
+              <ArrowRight className="ml-2 h-5 w-5" />
+            </Button>
+          </div>
+
+          <div className="mt-8 flex items-center gap-4 rounded-lg border-2 border-gray-300 bg-gray-200 px-6 py-4">
+            <img
+              className="size-20 select-none"
+              src="/public/img/innova-gray-400.webp"
+              alt="img"
+            />
+            <p className="font-medium text-gray-400">
+              Nosso compromisso é desenvolver projetos personalizados, realizar
+              manutenções precisas e implementar tecnologias que otimizam
+              processos, reduzem custos e aumentam a produtividade.
+            </p>
+          </div>
+        </div>
+      </div>
+      <BrasilBanner />
+    </section>
+  );
+}
